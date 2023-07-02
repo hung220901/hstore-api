7651c091131ed6da51c139d6a4c49bd33831f2e2
hung220901 <hung220901@gmail.com>
2023-07-02 07:19:06 +0700

    updating handle token

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -3,14 +3,28 @@ const jwt = require('jsonwebtoken');
 
 
 exports.verifyToken = (req,res,next)=>{
-    const Authorization = req.header('authorization');
-    if(!Authorization){
-        const err = new Error('Unauthorized!');
-        err.statusCode = 401;
-        return next(err);
+    const token = req.headers.token;
+    if(token){
+        const acessToken =  token.split(" ")[1];
+        jwt.verify(acessToken, process.env.APP_SECRET,(err,user)=>{
+            if(err){
+                res.status(403).json("Token is not valid")
+            }
+            res.user = user;
+            next()
+        })
     }
-    const token = Authorization.replace("Bearer ", "");
-    const {userId} = jwt.verify(token, process.env.APP_SECRET);
-    req.user = {userId};
-    next();
+    else{
+        res.status(401).json("Unauthorize!")
+    }
+}
+exports.verifyAndAdminAuth = (req, res, next)=>{ 
+    this.verifyToken(req,res,()=>{
+        if(req.user.id == req.params.id || req.user.role === "admin"){
+            next()
+        } 
+        else{
+            req.status(403).json("You're not allowed to do that")
+        }
+    });
 }
